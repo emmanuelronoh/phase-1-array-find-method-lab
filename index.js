552ffa7aee78814ec35aa296529f87b5a9ea8f96
emmanuel ronoh <eronoh036@gmail.com>
2024-07-02 13:06:18 +0300

    first commit

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // code your solution here
+// helpers.js
+
+function superbowlWin(record) {
+    // Search for the first entry where result is "W"
+    const winningYear = record.find(entry => entry.result === "W");
+    
+    // If found, return the year
+    // If not found, return undefined
+    return winningYear ? winningYear.year : undefined;
+  }
+  
+  export default superbowlWin;
